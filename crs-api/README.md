60d47d79ae31b3a898211c9a2607c62c48b1fb52
Jordan <jmmhqw@mst.edu>
2023-10-04 18:50:08 -0500

    Django working (#7)
    * Started django files
    
    * Updated readme, removed vscode stuff
    
    * Removed DB folder since DB is now hosted on neon.tech
    
    * Removed VScode settings from repo
    
    trying to remove vscode stuff
    
    * Delete .vscode directory
    
    * Changed ID's to auto and added migrations
    
    * Added Workflow to verify backend
    
    UPdated workflow
    
    UPdated workflow
    
    UPdated workflow
    
    Updated workflow
    
    Updated workflow
    
    * Removed PWD step
    
    Was only used for debugging the action

--- crs-api/README.md
+++ crs-api/README.md
@@ -1,10 +1,17 @@
-# Python Flask REST API (crs-api)
+# Python Django REST API (crs-api)
 
 ## Running the API
 
-1. Install pipenv: `pip install pipenv`
-2. Install dependencies: `pipenv install`
-3. Activate the virtual environment: `pipenv shell`
-4. Run the API: `flask run -h 0.0.0.0 -p 5001`
+1. Install `virtualenv` using `pip install virtualenv`
+2. Create a virtual python environment using `virtualenv venv`
+3. Activate the virtual environment. This step varies depending on the OS, so run the command relevaten to your system:
 
-The API will be available at `http://localhost:5001/`.
+   > Bash: `source ./venv/bin/activate`
+   >
+   > Fish: `source ./venv/bin/activate.fish`
+   >
+   > Windows: `./venv/Scripts/activate`
+
+4. Install dependencies to the virtual environment using `pip install -r requirements.txt`
+5. Start the API with `python manage.py runserver`
+6. Optionally, you can choose the port to run the server on by adding a port after runserver, e.x. `python manage.py runserver 5001`
---
+++ crs-api/django_api/models.py
@@ -0,0 +1,62 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    num_people = models.IntegerField()
+    name = models.CharField(max_length=50, blank=True, null=True)
+    room = models.ForeignKey("Room", models.DO_NOTHING, db_column="room")
+    created_by = models.ForeignKey("User", models.DO_NOTHING, db_column="created_by")
+
+    class Meta:
+        managed = False
+        db_table = "Booking"
+
+
+class Building(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    floors = models.IntegerField()
+    open_time = models.TimeField()
+    close_time = models.TimeField()
+
+    class Meta:
+        managed = False
+        db_table = "Building"
+
+
+class Room(models.Model):
+    id = models.AutoField(primary_key=True)
+    identifier = models.CharField(max_length=50)
+    capacity = models.IntegerField()
+    size = models.IntegerField(blank=True, null=True)
+    tv = models.BooleanField()
+    projector = models.BooleanField()
+    whiteboard = models.BooleanField()
+    computers = models.IntegerField()
+    building = models.ForeignKey(Building, models.DO_NOTHING, db_column="building")
+
+    class Meta:
+        managed = False
+        db_table = "Room"
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = "User"
