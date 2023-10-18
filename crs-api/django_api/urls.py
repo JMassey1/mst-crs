c077a02df533b4e97d46dc2d9b4c27fff7c1e188
Josh-H-00 <joshua.hagstrom.00@gmail.com>
2023-10-18 15:10:09 -0500

    Django router to handle database HTTP requests

--- crs-api/django_api/urls.py
+++ crs-api/django_api/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
-urlpatterns = [path("admin/", admin.site.urls), path("", views.index, name="index")]
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'Room', views.RoomView,'rooms')
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
+    path("api/", include(router.urls))
+]
--- crs-api/django_api/views.py
+++ crs-api/django_api/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 
+from rest_framework import viewsets
+from .serializers import RoomSerializer
+from .models import Room
+
 
 def index(request):
     return HttpResponse("API is working!! :D")
+
+class RoomView(viewsets.ModelViewSet):
+    serializer_class = RoomSerializer
+    queryset = Room.objects.all()
