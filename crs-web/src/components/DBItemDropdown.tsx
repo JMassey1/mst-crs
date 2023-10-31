48af1f456c4d35e3623f63934a3054adea4c29e0
Jordan Massey <jmmhqw@mst.edu>
2023-10-31 09:57:50 -0500

    Fixed dropdown, data displayed properly

--- crs-web/src/components/DBItemDropdown.tsx
+++ crs-web/src/components/DBItemDropdown.tsx
@@ -1,54 +1,54 @@
-import { useState } from "react";
-import AsyncSelect from "react-select";
+import { useState, useEffect } from "react";
+import AsyncSelect from "react-select/async";
+import Select from "react-select";
 import api from "./../api/api";
 
+import { Building } from "../types/DB_Types";
+
 function DBItemDropdown() {
-    const[items, setItems] = useState([]);
-    const[inputValue, setValue] = useState('');
-    const[selectedValue, setSelectedValue] = useState(null);
+  const [inputValue, setValue] = useState("");
+  const [selectedValue, setSelectedValue] = useState(null);
 
-    // handle input change event
-    const handleInputChange = value => {
-        setValue(value);
-    };
+  // handle input change event
+  const handleInputChange = (value: any) => {
+    setValue(value);
+  };
 
-    // handle selection
-    const handleChange = value => {
-        setSelectedValue(value);
-    }
+  // handle selection
+  const handleChange = (value: any) => {
+    setSelectedValue(value);
+  };
 
-    const fetch = () => {
-        return api.get("").then((response) => {
-            const options = [];
-            response.data.forEach((building) => {
-                options.push({
-                    name: building.name,
-                    id: building.id
-                })
-            });
-            const opt = response;
-            return opt;
+  const fetch = () => {
+    return api.get("buildings").then((response) => {
+      const options: any[] = [];
+      response.data.forEach((building: Building) => {
+        options.push({
+          label: building.name,
+          value: building.id,
         });
-    }
+      });
+      return options;
+    });
+  };
 
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-4">
-                    <AsyncSelect
-                        cacheOptions
-                        defaultOptions
-                        value={selectedValue}
-                        getOptionLabel={e => e.name}
-                        getOptionValue={e => e.id}
-                        loadOptions={fetch}
-                        onInputChange={handleInputChange}
-                        onChange={handleChange}
-                    />
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <AsyncSelect
+            cacheOptions
+            defaultOptions
+            value={selectedValue}
+            loadOptions={fetch}
+            onInputChange={handleInputChange}
+            onChange={handleChange}
+            isClearable={true}
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
-export default DBItemDropdown
+export default DBItemDropdown;
---
+++ crs-web/src/types/DB_Types.ts
@@ -0,0 +1,8 @@
+export type Building = {
+  address: string;
+  close_time: string;
+  floors: number;
+  id: number;
+  name: string;
+  open_time: string;
+};
